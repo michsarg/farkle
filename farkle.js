3d0ab0131255aae17a3cabab5d7c75eea9689af3
michsarg <22737938@student.uwa.edu.au>
2021-06-29 20:24:40 +0800

    game logic developed

--- farkle.js
+++ farkle.js
@@ -8,10 +8,12 @@
 
 
 // VARIABLES
-p1Score = 0; // formerly userScore
-p2Score = 0; // formerly opponentScore
-activeHand = [1, 1, 1, 1, 1, 1];
+p1Total = 0;
+p1Round = 0;
+p1Selected = 0;
+activeHand = [0, 0, 0, 0, 0, 0];
 selectedDie = [false, false, false, false, false, false];
+bankedDie = [false, false, false, false, false, false];
 roll();
 
 // FUNCTIONS
@@ -19,13 +21,26 @@ function roll() {
     console.log("activeHand: " + activeHand);
     console.log("selectedDie: " + selectedDie);
     for (let i = 0; i < 6; i++) {
-        if (selectedDie[i] == false) {
+        if (selectedDie[i] == false && bankedDie[i] == false) {
             activeHand[i] = getDie(i);
         }
     }
     updateView();
-    // getScore(this.hand);
+    // check for bust
 }
+
+function bustCheck() {
+
+    dice = document.getElementsByClassName("die");
+    for (let i = 0; i < 6; i++) {
+
+    }
+
+    alert(p1Selected);
+
+
+}
+
 
 // updates webpage with data structure values
 function updateView() {
@@ -72,14 +87,16 @@ function getDie(position) {
 };
 
 
+
 // Returns score of selected die
+// called when a die is clicked
 function getScore(passedHand) {
 
     // deep copy passed hand
     let inputHand = JSON.parse(JSON.stringify(passedHand));
-
-    let score = 0;
     let dieCount = [0, 0, 0, 0, 0, 0];
+    let score = 0;
+
     for (let i = 0; i < inputHand.length; i++) { // traverse die values
         for (let j = 0; j < 6; j++) { // traverse counter values
             if (inputHand[i] == (j + 1)) { dieCount[j]++; }
@@ -113,23 +130,45 @@ function getScore(passedHand) {
     // doesn't cover multiple triples of same
     for (let i = 0; i < dieCount.length; i++) {
         if (dieCount[i] >= 3) {
-            let multiplier = Math.floor(dieCount[i] / 3);
-            score += ((i + 1) * 100 * multiplier);
-            dieCount[i] = -(3 * multiplier);
-            console.log(multiplier + "x triple " + (i + 1));
+            score += ((i + 1) * 100);
+            dieCount[i] -= 3;
+            console.log("triple " + (i + 1));
         }
         // single ones
         else if (i == (1 - 1) && dieCount[i] > 0) {
             score += ((dieCount[i]) * 100);
             console.log(dieCount[i] + "x single ones");
+            //reset dieCount
+            dieCount[i] = 0;
         }
         //single fives
         else if (i == (5 - 1) && dieCount[i] > 0) {
             score += ((dieCount[i]) * 50);
             console.log(dieCount[i] + "x single fives");
+            // bank selected scoring die
+            //reset dieCount
+            dieCount[i] = 0;
         }
     }
+
+    // html update
+    // Disable options if non-scoring die are selected
+    let nonScoreDie = 0;
+    for (let i = 0; i < 6; i++) {
+        if (dieCount[i] != 0) {
+            nonScoreDie++;
+        }
+    }
+    if (nonScoreDie > 0) {
+        document.getElementById("scoreRoll").classList.add("disabled");
+        document.getElementById("scorePass").classList.add("disabled");
+    } else {
+        document.getElementById("scoreRoll").classList.remove("disabled");
+        document.getElementById("scorePass").classList.remove("disabled");
+    }
+
     console.log("score: " + score);
+    console.log(dieCount);
     return score;
 }
 
@@ -137,12 +176,17 @@ function getSelectedScore() {
     selectedHand = [];
     // get only selected values
     for (let i = 0; i < 6; i++) {
-        if (selectedDie[i] == true) {
+        if (selectedDie[i] == true && bankedDie[i] == false) {
             selectedHand[i] = activeHand[i];
         }
     }
-    let score = getScore(selectedHand);
-    return score;
+    return getScore(selectedHand);
+}
+
+function updateScoreBoard() {
+    document.getElementById("p1Total").innerHTML = p1Total;
+    document.getElementById("p1Round").innerHTML = p1Round;
+    document.getElementById("p1Selected").innerHTML = p1Selected;
 }
 
 
@@ -151,28 +195,40 @@ $(document).ready(function() {
 
     updateView();
 
-    // listener for roll button/method
-    $("#scoreRoll").click(function() {
-        roll();
+    $("#quit").click(function() {
+        bustCheck();
     });
 
-    // listener for score button/method
-    $("#scoreDie").click(function() {
-        selectedHand = [];
-        // get only selected values
+    // listener for scoreRoll button/method
+    // can assume that only scoring dice have been selected
+    $("#scoreRoll").click(function() {
+        dice = document.getElementsByClassName("die");
         for (let i = 0; i < 6; i++) {
             if (selectedDie[i] == true) {
-                selectedHand[i] = activeHand[i];
+                bankedDie[i] = true;
+                // update html display to show banked
+                dice[i].classList.remove("selected");
+                dice[i].classList.add("banked");
             }
         }
-        let score = getScore(selectedHand);
-        p1Score += score;
-        console.log("scored as: " + score);
-        updateView();
+        console.log("banked: " + bankedDie);
+        // update scoreboard
+        p1Round += p1Selected;
+        p1Selected = 0;
+        updateScoreBoard();
+        // call to reroll unselected die
+        roll();
+
     });
 
     // listener for dice click/selection
     $(".die").click(function() {
+        // check die not banked
+        if ((this).classList.contains("banked")) {
+            console.log("is this banked? " + (this).classList.contains("banked"))
+            return
+        }
+
         // update class of clicked die
         (this).classList.toggle("selected");
         (this).classList.toggle("notSelected");
@@ -185,11 +241,8 @@ $(document).ready(function() {
         console.log("selectedDie[pos]: " + selectedDie[pos]);
 
         //update selected menu value
-        let selectedScore = getSelectedScore();
-        let selected = document.getElementById("p1Selected");
-        selected.innerHTML = "";
-        selected.innerHTML = selectedScore;
-
+        p1Selected = getSelectedScore();
+        updateScoreBoard();
 
     });
 
